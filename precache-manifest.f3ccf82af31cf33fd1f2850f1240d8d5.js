3097df00b94520dc86608f60303ebf010852f13d
Alex Yu <alex.yurchenko@gmail.com>
2020-01-17 10:53:18 +0200

    Updates

---
+++ precache-manifest.f3ccf82af31cf33fd1f2850f1240d8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfae823652e3fa70d94e53159020fa9",
+    "url": "/goit-react-app-task/index.html"
+  },
+  {
+    "revision": "20917bd08d5b3ba2ba9a",
+    "url": "/goit-react-app-task/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d720571b1f13133156d7",
+    "url": "/goit-react-app-task/static/js/2.7d7472e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-app-task/static/js/2.7d7472e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20917bd08d5b3ba2ba9a",
+    "url": "/goit-react-app-task/static/js/main.13b8e0d5.chunk.js"
+  },
+  {
+    "revision": "ada9d3f9be5074c313ce",
+    "url": "/goit-react-app-task/static/js/runtime-main.7b2d7f63.js"
+  }
+]);
